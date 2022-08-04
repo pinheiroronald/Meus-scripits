842ead59026d05270135603ac21c024b6a7ee2d1
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2022-08-04 13:22:34 -0300

    creat

---
+++ teste de objetos/testeob1.js
@@ -0,0 +1,11 @@
+let inf_cliente = []
+let nome = ["Ronald","Romeu","Amanda","Carlos","Samuel","Raiane"]
+let tele =["969897654","832459087","834256768","981234567","879045344"]
+let cadastrando = {nome2:"",fon:""}
+
+for(let i = 0; i < 5; i++){
+    cadastrando.nome2 = nome[i]
+    cadastrando.fon = tele[i]
+    inf_cliente.push(cadastrando)
+
+}
