ef39fbfbb81bf453f5fd1d4820b97ae7a8b3f08c
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2022-08-04 21:37:52 -0300

    atu

--- teste de objetos/testeob1.js
+++ teste de objetos/testeob1.js
@@ -1,4 +1,2 @@
-let inf_cliente = []
-let nome = ["Ronald","Romeu","Amanda","Carlos","Samuel","Raiane"];
-let tele =["969897654","832459087","834256768","981234567","879045344"];
-
+var nome = "ronald"
+console.log(nome)
